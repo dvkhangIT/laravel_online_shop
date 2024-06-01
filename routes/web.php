0cb95f0736758d7afa9deea2d5c728d29c8bd3fd
dvkhangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-06-01 19:03:59 +0700

    Admin Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middlewate' => 'admin.guest'], function () {
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+    });
+    Route::group(['middlewate' => 'admin.auth'], function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+    });
 });
